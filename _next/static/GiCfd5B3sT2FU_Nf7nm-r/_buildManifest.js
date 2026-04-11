fd23059e263aaad631485a31f3a2f48172bb65cb
Mikal Monslaup <mikalmonslaup@Mikals-MBP.home>
2026-04-11 07:04:08 +0200

    Updates

---
+++ _next/static/GiCfd5B3sT2FU_Nf7nm-r/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/boost-ai-interactive-guide//_next/:path+",
+        "destination": "/boost-ai-interactive-guide/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
